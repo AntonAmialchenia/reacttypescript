067e38aff626e2f90d24052e086af0b6de01c590
Антон Амельченя <107065275+AntonAmialchenia@users.noreply.github.com>
2023-01-14 20:35:27 +0300

    Typescript + react

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import Card, { CardVariant } from './components/Card';
+import List from './components/List';
+import { IUser, ITodo } from './types/types';
+import UserItem from './components/UserItem';
 
 function App() {
+  const [users, setUsers] = useState<IUser[]>([]);
+  const [todos, setTodos] = useState<ITodo[]>([]);
+
+  useEffect(() => {
+    fetchUsers();
+    fetchTodos();
+  }, []);
+
+  async function fetchUsers() {
+    try {
+      const response = await axios.get<IUser[]>(
+        'https://jsonplaceholder.typicode.com/users'
+      );
+      setUsers(response.data);
+    } catch (e) {
+      alert(e);
+    }
+  }
+
+  async function fetchTodos() {
+    try {
+      const response = await axios.get<ITodo[]>(
+        'https://jsonplaceholder.typicode.com/todos?_limit=10'
+      );
+      setTodos(response.data);
+    } catch (e) {
+      alert(e);
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Card
+        onClick={(num) => console.log('click', num)}
+        variant={CardVariant.outlined}
+        width="280px"
+        height="280px"
+      >
+        <button>кнопка</button>
+        <div>Hello!</div>
+      </Card>
+      <List
+        items={users}
+        renderItem={(user: IUser) => <UserItem user={user} key={user.id} />}
+      />
+    </>
   );
 }
 
